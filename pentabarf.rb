2ac5a56f33487793179e9c1b63a48d2424063cb7
Alexander Klink <git@alech.de>
2009-10-24 20:50:30 +0200

    more Pentabarf XML parsing, complete except for sorting the events

--- pentabarf.rb
+++ pentabarf.rb
@@ -2,14 +2,23 @@ require 'rubygems'
 require 'open-uri'
 require 'hpricot'
 
+
+module HpricotAttributes
+    def set_attributes_from_hpricot(h)
+        h.children.select { |c| c.class == Hpricot::Elem }.each do |c|
+            self.instance_variable_set "@#{c.name}".to_sym, c.inner_text
+        end
+    end
+end
+
 module Pentabarf
     class Conference
+        include HpricotAttributes
         attr_reader :title, :subtitle, :venue, :city, :start, :end, :days,
                     :release, :day_change, :timeslot_duration, :events
         def initialize(args)
             uri      = args[:uri]
             fallback = args[:fallback]
-            puts fallback
             h = 
             begin
                 h = open(uri) { |f| Hpricot(f) }
@@ -20,21 +29,40 @@ module Pentabarf
 
             # set attributes from the corresponding XML part
             conf = h.at('//schedule/conference')
-            conf.children.select { |c| c.class == Hpricot::Elem }.each do |c|
-                puts c.inner_text
-                puts c.name
-                self.instance_variable_set "@#{c.name}".to_sym, c.inner_text
+            set_attributes_from_hpricot(conf)
+            @events = []
+            days = h.search('//schedule/day')
+            days.each do |day|
+                date   = day.attributes['date']
+                events = day.search('event')
+                events.each do |event|
+                    @events.push(Event.new(:event => event, :date => date))
+                end
             end
-            puts self.title
-            puts self.subtitle
+#            @events.each { |e| puts e.class }
+#            @events.sort! # FIXME
+#            puts @events.inspect
         end
     end
     
     class Event
         include Comparable
+        include HpricotAttributes
         attr_reader :id, :date, :start, :duration, :room, :tag, :title,
                     :subtitle, :track, :type, :language, :abstract,
                     :description, :persons
+
+        def initialize(args)
+            event = args[:event]
+            @id = event.attributes['id']
+            date  = args[:date]
+            set_attributes_from_hpricot(event)
+            # persons need to be dealt with separately
+            @persons = []
+            event.search('persons/person').each do |p|
+                @persons.push(Person.new(p.attributes['id'], p.inner_text))
+            end
+        end
 
         def <=>(other)
             # compare date first
@@ -45,13 +73,18 @@ module Pentabarf
             if (self.start <=> other.start) != 0 then
                 return self.start <=> other.start
             end
-            # the room number
+            # then room number
             self.room <=> other.room
         end
     end
 
     class Person
         attr_reader :id, :name
+
+        def initialize(id, name)
+            @id   = id
+            @name = name
+        end
     end
 end
 

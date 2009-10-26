b84c404796a22a04f142ea32174c932619d8eecd
Alexander Klink <git@alech.de>
2009-10-26 20:36:54 +0100

    current block/upcoming block implemented, filenames for events/persons, day_change take into consideration

--- pentabarf.rb
+++ pentabarf.rb
@@ -1,7 +1,8 @@
 require 'rubygems'
 require 'open-uri'
 require 'hpricot'
-
+require 'time'
+require 'digest/sha2'
 
 module HpricotAttributes
     def set_attributes_from_hpricot(h)
@@ -29,7 +30,7 @@ module Pentabarf
     class Conference
         include HpricotAttributes
         attr_reader :title, :subtitle, :venue, :city, :start, :end, :days,
-                    :release, :day_change, :timeslot_duration, :events
+                    :release, :day_change, :timeslot_duration, :events, :rooms
         def initialize(args)
             uri      = args[:uri]
             fallback = args[:fallback]
@@ -50,10 +51,45 @@ module Pentabarf
                 date   = day.attributes['date']
                 events = day.search('event')
                 events.each do |event|
-                    @events.push(Event.new(:event => event, :date => date))
+                    @events.push(Event.new(:event => event,
+                                           :date  => date,
+                                           :day_change => @day_change))
                 end
             end
             @events.sort!
+            @rooms = @events.map { |e| e.room }.uniq.sort
+        end
+
+        def current_block(date = Time.now)
+            result = []
+            @rooms.each do |room|
+                current = @events.select do |e|
+                    (e.room == room) && (e.start <= date) && (e.end >= date)
+                end
+                if current.size > 1 then
+                    raise RuntimeError,
+                        "more than one event in room #{room} at #{date}!"
+                end
+                if current.size == 1 then
+                    result.push current.first
+                end
+            end
+            result
+        end
+
+        def upcoming_block(date = Time.now, max_date = date + 60*60*5)
+            result = []
+            @rooms.each do |room|
+                current = @events.select do |e|
+                    (e.room == room) &&
+                    (e.start > date) &&
+                    (e.start <= max_date)
+                end
+                if current.size >= 1 then
+                    result.push current.first
+                end
+            end
+            result
         end
     end
     
@@ -62,31 +98,47 @@ module Pentabarf
         include HpricotAttributes
         attr_reader :id, :date, :start, :duration, :room, :tag, :title,
                     :subtitle, :track, :type, :language, :abstract,
-                    :description, :persons
+                    :description, :persons, :end
 
         def initialize(args)
-            event = args[:event]
-            @id   = event.attributes['id']
-            @date = args[:date]
+            event      = args[:event]
+            @id        = event.attributes['id']
+            @date      = args[:date]
+            day_change = args[:day_change]
+
             set_attributes_from_hpricot(event)
             # persons need to be dealt with separately
             @persons = []
             event.search('persons/person').each do |p|
                 @persons.push(Person.new(p.attributes['id'], p.inner_text))
             end
+            # the day in Pentabarf does not stop at 23:59, but at the
+            # "day_change" conference parameter. This means that events
+            # at day x before day_change are actually at day x + 1.
+            if @start < day_change then
+                @date = (Date.parse(@date) + 1).strftime
+            end
+            # use Time objects for start and end
+            @start = Time.parse "#{@date} #{@start}" 
+            duration_hours, duration_minutes =
+                @duration.split(':').map { |e| e.to_i }
+            @end = @start + duration_minutes * 60 + duration_hours * 60 * 60
         end
 
         def <=>(other)
-            # compare date first
-            if (self.date <=> other.date) != 0 then
-                return self.date <=> other.date
-            end
-            # then starting time
+            # compare starting time
             if (self.start <=> other.start) != 0 then
                 return self.start <=> other.start
             end
             # then room number
             self.room <=> other.room
+        end
+
+        def to_filename(extension)
+            # add a hash over title and subtitle, as we might need to
+            # regenerate the file if that information changes
+            digest = Digest::SHA256.new().update(@title + @subtitle).to_s[1..10]
+            "#{@id}_#{digest}.#{extension}"
         end
     end
 
@@ -97,8 +149,24 @@ module Pentabarf
             @id   = id
             @name = name
         end
+
+        def to_filename(extension)
+            digest = Digest::SHA256.new().update(@name).to_s[1..10]
+            "#{@id}_#{digest}.#{extension}"
+        end
     end
 end
 
 conf = Pentabarf::Conference.new(:uri => 'http://fwef8uwe9fw',
                                  :fallback => 'sample_schedule.xml')
+
+puts conf.current_block(Time.now).inspect
+puts conf.current_block(Time.parse("2009-12-27 10:00")).inspect
+puts conf.current_block(Time.parse("2009-12-27 20:30")).inspect
+
+puts conf.upcoming_block(Time.now).inspect
+conf.upcoming_block(Time.parse("2009-12-27 09:50")).each do |e|
+    puts e.title
+    puts e.subtitle
+    puts e.to_filename('gsm')
+end

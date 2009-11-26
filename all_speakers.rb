bc72acb2d3e6e7bcd987a601a53631b4aa43214a
Alexander Klink <git@alech.de>
2009-11-26 22:30:44 +0100

    all_speakers.rb: output sorted

--- all_speakers.rb
+++ all_speakers.rb
@@ -1,11 +1,14 @@
 require 'pentabarf'
 conf = Pentabarf::Conference.new(:uri => 'http://fwef8uwe9fw',
                                  :fallback => '68.xml')
+person_map = {}
 conf.events.each do |e|
 #    puts e.title + " - " + e.subtitle
     e.persons.each do |p|
  #        puts "\e[1;31m#{p.name}\e[0m"
-         puts "#{p.name}\n#{p.id}"
+#         puts "#{p.name}\n#{p.id}"
          #puts p.id_hash + '.wav'
+         person_map[p.name] = p.id
     end
 end
+person_map.sort.each { |k,v| puts k; puts v }

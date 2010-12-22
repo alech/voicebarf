b0a4699ad5ddd78f7cf55ff0905e122ee9196f02
Alexander Klink <git@alech.de>
2010-12-22 09:48:53 +0100

    updated script to list all events for rcording for 27C3

--- all_events.rb
+++ all_events.rb
@@ -1,13 +1,13 @@
 require 'pentabarf'
-conf = Pentabarf::Conference.new(:uri => 'http://events.ccc.de/congress/2009/Fahrplan/schedule.en.xml',
-                                 :fallback => '68.xml')
+conf = Pentabarf::Conference.new(:uri => 'schedule-184773.xml')
 speakers = {}
 conf.events.each do |e|
+	puts e.start
+	puts "#{e.title} - #{e.title_id_hash}"
+	puts "#{e.subtitle} - #{e.subtitle_id_hash}" if e.subtitle != ""
     e.persons.each do |p|
-        speakers[p.name] = [ p, e.title ]
+		puts "#{p.name} - #{p.id_hash}"
     end
+	puts
 end
 
-speakers.each do |name, p|
-    puts "mv #{p[0].id}.wav ../speaker/#{p[0].id_hash}.wav"
-end
